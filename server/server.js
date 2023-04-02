1a46885f727fb200f64507f06b2ed4b9bbba1f37
Amisha8899 <105807890+Amisha8899@users.noreply.github.com>
2023-04-03 00:03:11 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const Document = require("./document")
+mongoose.connect('mongodb://localhost:27017/inscribe',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).catch((error) => console.log(error));
+const defaultValue = "";
+const io = require('socket.io')(3001,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"],
+    },
+})
+io.on("connection",socket=>{
+    socket.on('get-document', async documentId =>{
+        const document = await findOrCreateDocument(documentId)
+        socket.join(documentId)
+        socket.emit('load-document', document.data)
+        socket.on('send-changes',delta =>{
+            socket.broadcast.to(documentId).emit('receive-changes',delta)
+        })
+        socket.on("save-document", async data =>{
+            await Document.findByIdAndUpdate(documentId,{data})
+        })
+    })
+    console.log("connected");
+})
+
+async function findOrCreateDocument(id){
+    try{
+    if(id==null) return
+    const document = await Document.findById(id)
+    if(document) return document
+    return await Document.create({_id: id, data: defaultValue})
+    }
+    catch(error){
+        console.log(error);
+    }
+}
